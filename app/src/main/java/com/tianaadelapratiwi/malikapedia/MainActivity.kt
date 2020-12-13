5ae0f359efc247f2a0d74bc55ba54ddcbf386cc5
tianaadelapratiwi <71165318+tianaadelapratiwi@users.noreply.github.com>
2020-12-13 20:11:56 +0700

    Initial commit

---
+++ app/src/main/java/com/tianaadelapratiwi/malikapedia/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tianaadelapratiwi.malikapedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.belajar_linear_layout)
+    }
+}
